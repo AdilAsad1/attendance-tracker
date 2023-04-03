d791657459f48d84a76b759e42ad00c08aa2d3ee
Adil <78382272+AdilAsad1@users.noreply.github.com>
2023-04-03 15:41:29 -0400

    added roomdatabase

---
+++ app/src/main/java/db/ClassDao.java
@@ -0,0 +1,22 @@
+import androidx.lifecycle.LiveData;
+import androidx.room.Dao;
+import androidx.room.Insert;
+import androidx.room.OnConflictStrategy;
+import androidx.room.Query;
+
+import java.util.List;
+
+@Dao
+public interface ClassDao {
+    @Query("SELECT * FROM class_list")
+    LiveData<List<Class>> getAllClasses();
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    void insertClass(Class... class_);
+
+    @Query("DELETE FROM class_list WHERE id = :classId")
+    void deleteClass(int classId);
+
+
+}
+
---
+++ app/src/main/java/db/StudentDao.java
@@ -0,0 +1,19 @@
+import androidx.lifecycle.LiveData;
+import androidx.room.Dao;
+import androidx.room.Insert;
+import androidx.room.OnConflictStrategy;
+import androidx.room.Query;
+
+import java.util.List;
+
+@Dao
+public interface StudentDao {
+    @Query("SELECT * FROM students WHERE class_id = :classId")
+    LiveData<List<Student>> getStudentsByClassId(int classId);
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    void insertStudent(Student student);
+
+    @Query("DELETE FROM students WHERE id = :studentId")
+    void deleteStudent(int studentId);
+}
