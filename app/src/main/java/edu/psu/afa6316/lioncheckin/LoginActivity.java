9e00c87dd275c7c29501b4c66689afd3b5312f2e
ruchadaware18 <ruchad18@gmail.com>
2023-04-16 14:17:25 -0400

    Insert class activity, Login Activity and changes in Main Activity

---
+++ app/src/main/java/edu/psu/afa6316/lioncheckin/LoginActivity.java
@@ -0,0 +1,32 @@
+package edu.psu.afa6316.lioncheckin;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login_page);
+
+
+        Button loginButton = findViewById(R.id.button_login);
+
+        loginButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                System.out.println("Im am here");
+                Toast.makeText(LoginActivity.this, "Login successful", Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(LoginActivity.this, AddClassActivity.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+}
