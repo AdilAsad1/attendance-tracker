3ab12de5fa838807c68f4e46e4cb3af14648c69a
Adil <78382272+AdilAsad1@users.noreply.github.com>
2023-04-16 15:28:27 -0400

    added room database

---
+++ app/src/main/java/edu/psu/afa6316/lioncheckin/Class.java
@@ -0,0 +1,45 @@
+package edu.psu.afa6316.lioncheckin;
+
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity(tableName = "class_list")
+public class Class {
+    public Class(int id, String className, String classCode) {
+        this.id = id;
+        this.className = className;
+        this.classCode = classCode;
+    }
+
+    @PrimaryKey(autoGenerate = true)
+    @ColumnInfo(name = "id")
+    public int id;
+
+    @ColumnInfo(name = "name")
+    public String className;
+
+    @ColumnInfo(name = "code")
+    public String classCode;
+
+
+    public int getId() {
+        return id;
+    }
+
+    public String getClassName() {
+        return className;
+    }
+
+    public void setClassName(String className) {
+        this.className = className;
+    }
+
+    public String getClassCode() {
+        return classCode;
+    }
+
+    public void setClassCode(String classCode) {
+        this.classCode = classCode;
+    }
+}
