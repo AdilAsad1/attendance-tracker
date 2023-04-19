49ef66be63d89055dcfc90df9b99b8d888813692
Adil <78382272+AdilAsad1@users.noreply.github.com>
2023-04-18 21:34:58 -0400

    login authentication with firebase

--- app/src/main/java/edu/psu/afa6316/lioncheckin/LoginActivity.java
+++ app/src/main/java/edu/psu/afa6316/lioncheckin/LoginActivity.java
@@ -1,12 +1,21 @@
 package edu.psu.afa6316.lioncheckin;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
+
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.android.gms.tasks.OnCompleteListener;
+
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -15,18 +24,49 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_page);
 
-
+        EditText userNameEditText = findViewById(R.id.loginPage_username);
+        EditText passwordEditText = findViewById(R.id.loginPage_password);
         Button loginButton = findViewById(R.id.button_login);
+        FirebaseAuth mAuth = FirebaseAuth.getInstance();
 
         loginButton.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view){
                 System.out.println("Im am here");
-                Toast.makeText(LoginActivity.this, "Login successful", Toast.LENGTH_SHORT).show();
-                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
-                startActivity(intent);
+                String username = userNameEditText.getText().toString();
+                String password = passwordEditText.getText().toString();
+                mAuth.signInWithEmailAndPassword(username, password).addOnCompleteListener(LoginActivity.this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            Intent i = new Intent(LoginActivity.this, MainActivity.class);
+                            startActivity(i);
+                            finish();
+                            Toast.makeText(LoginActivity.this, "Login successful", Toast.LENGTH_SHORT).show();
+                        } else {
+                            Toast.makeText(LoginActivity.this, "Login failed.", Toast.LENGTH_SHORT).show();
+                        }
+
+                    }
+
+                });
             }
         });
-
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
